56ad4d1f190467c010c4c091f6d37a42b5d3818a
Andrew Fry <andyfry01@gmail.com>
2016-05-06 10:50:35 -0400

    in the middle of deploying, switched over from sqlite to postgresql

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160504204144) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "videos", force: :cascade do |t|
     t.string   "videoId"
     t.string   "title"
